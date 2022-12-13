cdb82b88b49962bd749dbdf1001eb87a1935b5f0
pravallika <109818166+prpent-heroku@users.noreply.github.com>
2022-12-13 12:41:25 -0500

    Update app_1.py

--- app_1.py
+++ app_1.py
@@ -13,7 +13,7 @@ import streamlit as st
 import joblib,os
 import spacy
 import pandas as pd
-# nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_sm')
 import matplotlib.pyplot as plt 
 import matplotlib
 matplotlib.use("Agg")
